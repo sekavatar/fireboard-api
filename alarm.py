c4e1ffff6205111df635888500ec12293e9e5102
Arne Usadel <arne.usadel@googlemail.com>
2017-11-29 21:14:29 +0100

    WIP: initial commit for alarm creation

---
+++ alarm.py
@@ -0,0 +1,55 @@
+from xml.etree.ElementTree import Element, SubElement
+from xml.etree import ElementTree
+from xml.dom import minidom
+import urllib.request
+import argparse
+import time
+import sys
+
+
+def prettify(elem):
+    rough_string = ElementTree.tostring(elem)
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="   ", encoding="UTF-8")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Fireboard Einsatz erzeugen')
+    parser.add_argument('--external_number', dest='external_number', default='')  # a.k.a. Leitstellennummer
+    parser.add_argument('--uniqueid', dest='unique_id', default=str(int(time.time())))
+    parser.add_argument('--keyword', dest='keyword', default='')
+    parser.add_argument('--announcement', dest='announcement', default='')
+    parser.add_argument('--location', dest='location', default='')
+    parser.add_argument('--latitude', dest='latitude', default='')
+    parser.add_argument('--longitude', dest='longitude', default='')
+    parser.add_argument('--timestamp', dest='timestamp', default='')
+    parser.add_argument('--situation', dest='situation', default='')
+    parser.add_argument('--apikey', dest='api_key', default='')
+    args = parser.parse_args()
+    base_uri = 'https://login.fireboard.net/api?authkey=' + args.api_key + '&call=operation_data'
+
+    root = Element('fireboardOperation')
+    root.set('version', '1.0')
+    SubElement(root, 'uniqueId').text = args.unique_id
+
+    basic_data = SubElement(root, "basicData")
+    SubElement(basic_data, 'externalNumber').text = args.external_number
+    SubElement(basic_data, 'keyword').text = args.keyword
+    SubElement(basic_data, 'announcement').text = args.announcement
+    SubElement(basic_data, 'location').text = args.location
+    geo_location = SubElement(basic_data, 'geo_location')
+    SubElement(geo_location, 'latitude').text = args.latitude
+    SubElement(geo_location, 'longitude').text = args.longitude
+    timestamp_started = SubElement(basic_data, 'timestampStarted')
+    SubElement(timestamp_started, 'long').text = args.timestamp
+    SubElement(basic_data, 'situation').text = args.situation
+
+    pret = prettify(root)
+    print(pret)
+
+    with urllib.request.urlopen(base_uri, data=pret) as response:
+        print(response.read())
+
+
+if __name__ == "__main__":
+    sys.exit(main())
